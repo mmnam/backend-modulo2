dd352d145bc82def5d3e9562ae5f1eaa5e6b032a
mmnam <manuelmend92@gmail.com>
2022-10-03 13:59:20 -0500

    se aniade tabla users

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const { Sequelize } = require('sequelize');
+import { Sequelize, DataTypes } from 'sequelize';
 
 const sequelize = new Sequelize('sqlite:db.sqlite3');
 
 console.log('hoooola!')
+
+const User = sequelize.define("User", {
+    nombre: DataTypes.STRING,
+    appellidoPaterno: DataTypes.STRING,
+    appellidoMaterno: DataTypes.STRING,
+    correo: DataTypes.STRING,
+    rol: DataTypes.STRING
+});
