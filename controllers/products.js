958d32da7cfac48479c643cf403f47e7e66d4d24
Alopez-7 <alejandro.lopezgzz7@gmail.com>
2022-10-06 18:03:27 -0500

    products ok

--- controllers/products.js
+++ controllers/products.js
@@ -6,11 +6,32 @@ function addProduct(req, res) {
         res.status(201).json(product);
     });
 }
+async function getProduct(req, res) {
+    const id = req.params.id;
+    const product = await Product.findByPk(id);
+    res.status(200).json(product);
+}
 async function getProducts(req,res){
     console.log(req.auth)
     const products = await Product.findAll()
     res.status(200).json(products)    
 }
 
+async function deleteProduct(req, res) {
+    const id = req.params.id;
+    const deleted = Product.destroy(
+        {where: {id} }
+    );
+    res.status(200).json(deleted);
+}
 
-module.exports = { addProduct ,getProducts}
+async function updateProduct(req, res) {
+    const id = req.params.id;
+    const product = req.body;
+    await Product.update(product, {where: {id}});
+    const product_updated = await Product.findByPk(id);
+    res.status(200).json(product_updated);
+}
+
+
+module.exports = { addProduct ,getProducts,getProduct, deleteProduct,updateProduct}
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
-const { addProduct,getProducts } = require('../controllers/products');
+const { addProduct,getProducts ,deleteProduct,updateProduct,getProduct} = require('../controllers/products');
 const auth = require('../config/auth')
 
 
 
 router.post('/addProduct',auth.isSeller, addProduct);
 router.get('/getProducts',getProducts)
+router.patch('/:id', auth.isSeller,updateProduct);
+router.delete('/:id',auth.isSeller,deleteProduct)
+router.get('/:id',getProduct)
 
 module.exports = router; 
