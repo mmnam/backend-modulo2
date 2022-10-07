6cb276422f6e00df02c06ff9c429793bddfb9904
Alopez-7 <alejandro.lopezgzz7@gmail.com>
2022-10-07 04:15:43 -0500

    Reviews CRUD privileges for buyer/sellers

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,18 @@
 const passport = require('passport')
+const Reviews = require('../models/reviews')
 const localStrategy = require('passport-local').Strategy
 const User = require('../models/users')
 
 passport.use(new localStrategy({
     usernameField:'usuario',
     passwordField:'password'
-},function(email,password,done){
+},function(usuario,password,done){
     User.findOne({where:{usuario}}).then(function(user){
-        if(!user || !user.validtePassword(password)){
+        if(!user || !User.validatePassword(password,user.password_salt,user.password_hash)){
             return done(null,false,{errors:{'email o contrasena':'equivocado'}})
         }
+        
+        //if(user.id==Reviews.findByPk(req)){}
         return done(null,user)
     }).catch(done)
 }))
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { addProduct,getProducts ,deleteProduct,updateProduct,getProduct} = require('../controllers/products');
+const { addProduct,getProducts ,deleteProduct,updateProduct,getProduct,getProductByName} = require('../controllers/products');
 const auth = require('../config/auth')
 
 
 
 router.post('/addProduct',auth.isSeller, addProduct);
-router.get('/getProducts',getProducts)
+router.get('/',getProducts)
+router.get('/search',getProductByName)
 router.patch('/:id', auth.isSeller,updateProduct);
 router.delete('/:id',auth.isSeller,deleteProduct)
 router.get('/:id',getProduct)
